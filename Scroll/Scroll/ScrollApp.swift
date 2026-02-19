29707d29250d6fd93725ee4febec5bfaa7c0b26e
aBirrueta <alejandrobirru3ta@gmail.com>
2026-02-19 09:22:00 -0800

    Project started

---
+++ Scroll/Scroll/ScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollApp.swift
+//  Scroll
+//
+//  Created by Alejandro Birrueta on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct ScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Scroll/ScrollTests/ScrollTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollTests.swift
+//  ScrollTests
+//
+//  Created by Alejandro Birrueta on 2/18/26.
+//
+
+import Testing
+@testable import Scroll
+
+struct ScrollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
